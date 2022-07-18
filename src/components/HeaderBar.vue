0e69eeda4b3809e9ec926b7d05ef2f3d96e023d0
sbely <sbely@exadel.com>
2022-07-18 14:12:53 +0300

    feature: add simple HeaderBar component

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card color="grey lighten-4" flat tile>
+    <v-toolbar dense>
+      <v-img
+        class="mx-2"
+        src="../assets/logo.png"
+        max-height="40"
+        max-width="40"
+        contain
+      ></v-img>
+      <v-toolbar-title>The Mission</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text><router-link to="/">Home</router-link></v-btn>
+        <v-btn text><router-link to="/about">About</router-link></v-btn>
+        <v-btn text> Content </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "HeaderBar",
+};
+</script>
+
+<style scoped></style>
