7e59e6f2b5b55051d1f458618f68d145f3436d4e
sbely <sbely@exadel.com>
2022-09-06 14:11:48 +0000

    refactor: changed img size styles

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
   <v-main class="d-flex align-center fill-height">
     <v-container class="d-flex flex-column align-center" fluid>
-      <v-img src="../assets/logo.png" max-height="120" max-width="120" />
+      <v-img src="../assets/logo.png" height="120" width="120" />
       <Form class="col-12 col-sm-10 col-md-6" :title="headingName" />
       <v-btn text @click="redirectToRoute">{{ redirectButtonName }}</v-btn>
     </v-container>
