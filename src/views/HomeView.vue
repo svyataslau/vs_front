5fd36f8c848d6ec4fb8ca8f6345a3a1bd610184c
sbely <sbely@exadel.com>
2022-09-06 16:34:18 +0300

    fix: change refresh view bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,7 @@
     <v-row class="mt-2" no-gutters>
       <v-col cols="12" sm="10" md="8" lg="6" class="mx-auto">
         <v-row align="center" class="px-4 px-md-11 mx-0 my-4" no-gutters>
-          <v-col
-            v-if="isChallengesEmpty"
-            cols="12"
-            md="6"
-            class="text-center text-md-left my-4"
-          >
-            <h4 class="text-h4">My Challenges</h4>
-          </v-col>
-          <v-col v-else cols="12" class="text-center">
+          <v-col v-if="isChallengesEmpty" cols="12" class="text-center">
             <h4 class="text-h5 ma-4 font-weight-light">
               Nice to see you, {{ userNickname }}.
             </h4>
@@ -23,12 +15,15 @@
               To edit promises, click on the notepad button!
             </h4>
           </v-col>
+          <v-col v-else cols="12" md="6" class="text-center text-md-left my-4">
+            <h4 class="text-h4">My Challenges</h4>
+          </v-col>
 
           <v-col
             cols="12"
-            :md="isChallengesEmpty ? 6 : 12"
+            :md="isChallengesEmpty ? 12 : 6"
             class="text-center"
-            :class="{ 'text-md-end': isChallengesEmpty }"
+            :class="{ 'text-md-end': !isChallengesEmpty }"
           >
             <ChallengeDialog isLarge color="primary" :actionType="'create'">
               <v-icon>mdi-plus-thick</v-icon>
@@ -38,7 +33,7 @@
         </v-row>
 
         <v-sheet
-          v-if="isChallengesEmpty"
+          v-if="!isChallengesEmpty"
           class="ma-0 px-4 transparent"
           height="550"
         >
@@ -49,7 +44,7 @@
           />
         </v-sheet>
         <v-pagination
-          v-if="challenges.length > pageSize"
+          v-if="userChallenges.length > pageSize"
           v-model="page"
           circle
           class="mb-2"
@@ -79,54 +74,55 @@ export default Vue.extend({
       pageSize: 3,
       pageChallengeList: [] as UserChallenge[],
       isOverlayVisible: false,
-      challenges: [] as UserChallenge[],
+      isChallengesEmpty: true,
     };
   },
   computed: {
     ...mapGetters({
       userData: 'USER_DATA',
       isAdmin: 'IS_ADMIN',
+      userChallenges: 'USER_CHALLENGES',
     }),
     pages(): number {
-      return Math.ceil(this.challenges.length / this.pageSize);
+      return Math.ceil(this.userChallenges.length / this.pageSize);
     },
     userNickname(): string {
       return this.userData.nickname;
     },
-    isChallengesEmpty(): boolean {
-      return !!this.challenges.length;
-    },
   },
   watch: {
-    userData: {
-      handler(newUserData) {
-        if (newUserData.challenges?.length) {
-          this.challenges = [...newUserData.challenges].reverse();
-          this.initPage();
-          this.updatePage(this.page);
-        }
-      },
-      immediate: true,
+    userChallenges() {
+      this.updateView();
     },
-    pageChallengeList(newList) {
-      if (newList.length == 0) {
+  },
+  created() {
+    this.updateView();
+  },
+  methods: {
+    updateView() {
+      this.updatePage(this.page);
+      this.initPage();
+      if (this.pageChallengeList.length === 0) {
         --this.page;
         this.updatePage(this.page);
       }
-    },
-  },
-  methods: {
-    initPage() {
-      if (this.challenges.length < this.pageSize) {
-        this.pageChallengeList = this.challenges;
+      if (this.userChallenges.length === 0) {
+        this.isChallengesEmpty = true;
       } else {
-        this.pageChallengeList = this.challenges.slice(0, this.pageSize);
+        this.isChallengesEmpty = false;
+      }
+    },
+    initPage() {
+      if (this.userChallenges.length < this.pageSize) {
+        this.pageChallengeList = this.userChallenges;
+      } else {
+        this.pageChallengeList = this.userChallenges.slice(0, this.pageSize);
       }
     },
     updatePage(pageIndex: number) {
       const start = (pageIndex - 1) * this.pageSize;
       const end = pageIndex * this.pageSize;
-      this.pageChallengeList = this.challenges.slice(start, end);
+      this.pageChallengeList = this.userChallenges.slice(start, end);
       this.page = pageIndex;
     },
   },
